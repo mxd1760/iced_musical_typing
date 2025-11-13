54620ba004c582f510bd7ecd57cbded20a03ee97
Marcus Doucette <Marz3587@gmail.com>
2025-11-13 12:06:42 -0500

    update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use anyhow::Error;
 use iced::{
-    Element, Subscription, Theme, time,
-    widget::{column, text, text_input},
+    Element, Subscription, Task, Theme, time, widget::{column, row, text, text_input}
 };
-use std::time::{Duration, Instant};
+use rand::Rng;
+use std::{fs::File, io::{self, BufRead}, path::PathBuf, thread, time::{Duration, Instant}};
 
 pub fn main() -> iced::Result {
     iced::application("Typing Game", TypingGame::update, TypingGame::view)
@@ -22,6 +23,7 @@ struct TypingGame {
 enum Message {
     InputChanged(String),
     Tick(Instant),
+    UpdateTarget(String),
 }
 
 impl Default for TypingGame {
@@ -29,6 +31,8 @@ impl Default for TypingGame {
         Self::new()
     }
 }
+
+const MATCHING_COLOR:iced::Color = iced::Color::from_rgb(0.0, 0.5, 0.1);
 
 impl TypingGame {
     fn new() -> Self {
@@ -40,28 +44,67 @@ impl TypingGame {
         }
     }
 
-    fn update(&mut self, message: Message) {
+    fn update(&mut self, message: Message) ->Task<Message> {
         match message {
             Message::InputChanged(value) => {
                 self.input = value;
-                self.score = self
+                let num_matching = self
                     .input
                     .chars()
                     .zip(self.target.chars())
                     .take_while(|(a, b)| a == b)
                     .count();
-            }
-            Message::Tick(now) => {
-                if self.start_time.is_none() {
-                    self.start_time = Some(now);
+                if num_matching >= self.target.len(){
+                  self.score +=1;
+                  return Task::perform(
+                      async {
+                          load_new_line(format!("{}/src/main.rs", env!("CARGO_MANIFEST_DIR")), rand::rng().random::<i32>() as usize)
+                              .await
+                              .unwrap_or_else(|_| "Failed to load line".to_string())
+                      },
+                      Message::UpdateTarget,
+                  )
+            Message::UpdateTarget(new_target) => {
+                if is_valid_target_text(&new_target) {
+                    self.target = new_target;
+                    self.input = "".into();
+                } else {
+                    return Task::perform(
+                        async {
+                            load_new_line(format!("{}/src/main.rs", env!("CARGO_MANIFEST_DIR")), rand::rng().random::<i32>() as usize)
+                                .await
+                                .unwrap_or_else(|_| "Failed to load line".to_string())
+                        },
+                        Message::UpdateTarget,
+                    );
                 }
             }
+                self.target = new_target;
+                self.input = "".into();
+              }else{
+                return Task::perform(load_new_line(format!("{}/src/main.rs",env!("CARGO_MANIFEST_DIR")),rand::rng().random::<i32>() as usize),Message::UpdateTarget)
+              }
+            }
+            }
         }
+        Task::none()
     }
 
     fn view(&self) -> Element<Message> {
+      let num_matching = self
+                    .input
+                    .chars()
+                    .zip(self.target.chars())
+                    .take_while(|(a, b)| a == b)
+                    .count();
+        let matching_substr = &self.target[0..num_matching];
+        let remaining_substr = &self.target[num_matching..self.target.len()];
         column![
-            text(format!("Type this: {}", self.target)),
+            text("Type This:"),
+            row![
+                text(matching_substr).style(|_| text::Style { color: Some(MATCHING_COLOR) }),
+                text(remaining_substr)
+            ],
             text_input("Start typing...", &self.input).on_input(Message::InputChanged),
             text(format!("Score: {}", self.score)),
         ]
@@ -73,6 +116,28 @@ impl TypingGame {
     }
 
     fn theme(&self) -> Theme {
-        Theme::ALL[4].clone()
+        Theme::ALL[5].clone()
     }
+}
+
+async fn load_new_line(file_name: impl Into<PathBuf>,line_number:usize) -> anyhow::Result<String>{
+    let file_path = file_name.into();
+    let line_handle = thread::spawn(move || -> Result<String, io::Error> {
+      let file = File::open(file_path)?;
+      let reader = io::BufReader::new(file);
+      let lines:Vec<String> = reader.lines().filter_map(Result::ok).collect();
+      Ok(lines[line_number%lines.len()].clone())
+    });
+    
+    if let Ok(result) = line_handle.join(){
+      Ok(result?)
+    }else{
+      Err(anyhow::anyhow!("read file thread panicked"))
+    }
+}
+
+fn is_valid_target_text(text:&str)->bool{
+  let mut out = true;
+  out = out && text.len()>3;
+  out
 }
